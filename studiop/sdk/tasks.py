5a2ec1cf4f799fae8d1af77c2ed43b7b57de9998
Paul <dev@studiop.page>
2022-01-10 18:00:38 -0500

    Updated tasks module

--- studiop/sdk/tasks.py
+++ studiop/sdk/tasks.py
@@ -1,6 +1,6 @@
 import abc
 import pathlib
-from typing import Union
+from typing import List, Union
 
 from studiop import logging
 from studiop.sdk import archive, encrypt, upload
@@ -15,29 +15,27 @@ class Task(metaclass=abc.ABCMeta):
 class ArchiveTask(Task):
     def __init__(
         self,
-        src: Union[str, pathlib.Path],
+        source: Union[str, pathlib.Path],
         dest: str,
-        exclude: list[str] = None,
-        encrypt_: bool = True,
-        uploader: upload.Uploader = upload.S3Uploader,
-        archiver: archive.Archiver = archive.TarArchiver,
+        uploader: upload.Uploader,
+        archiver: archive.Archiver,
+        exclude: List[str] = None,
+        encryptor: encrypt.Encryptor = None,
     ) -> None:
-        self.src = pathlib.Path(src)
+        self.src = pathlib.Path(source)
         if not self.src.exists():
-            raise FileNotFoundError(f"Source file/folder '{src}' does not exist")
+            raise FileNotFoundError(f"Source file/folder '{source}' does not exist")
         self.dest = dest
         self._uploader = uploader
-        self.encrypt = encrypt_
         self._logger = logging.getLogger(self.__class__.__name__)
         self.exclude = exclude
         self._archiver = archiver
-        if self.encrypt:
-            self._encryptor = encrypt.TinkEncryptor("")
+        self._encryptor = encryptor
 
     def run(self):
         self._logger.info(f"Started archive task: {self.src}")
-        archived = self._archiver.archive(self.src)
-        if self.encrypt:
+        archived = self._archiver.archive(self.src, self.exclude)
+        if self._encryptor:
             archived = self._encryptor.encrypt(archived)
         self._uploader.upload(self.src.name, archived)
         self._logger.info(f"Completed archive task: {self.src}")
