72109ceca2e00b72f8f62b399f4f73adef197cc7
Paul <dev@studiop.page>
2022-01-10 18:00:13 -0500

    Updated archive module

--- studiop/sdk/archive.py
+++ studiop/sdk/archive.py
@@ -1,14 +1,33 @@
 import abc
+import fnmatch
+import os
 import pathlib
+import re
 import tarfile
 import tempfile
-from typing import BinaryIO, Union
+from typing import BinaryIO, Iterable, List, Union
 
-from studiop import logging
+from studiop import DRY_RUN, logging
+
+
+def create_filter(exclude: Iterable[str] = ()):
+    exclude_patterns = [fnmatch.translate(exp) for exp in exclude]
+    exclude_pattern = re.compile("|".join(exclude_patterns))
+
+    def filter_func(item: tarfile.TarInfo) -> tarfile.TarInfo:
+        if exclude_pattern.match(item.name):
+            return None
+        else:
+            print(f"Adding item: {item.name}")
+            return None if DRY_RUN else item
+
+    return filter_func
 
 
 class Archiver(metaclass=abc.ABCMeta):
-    def archive(self, src: Union[str, pathlib.Path]) -> BinaryIO:
+    def archive(
+        self, src: Union[str, pathlib.Path], exclude: List[str] = None
+    ) -> BinaryIO:
         raise NotImplementedError
 
 
@@ -18,14 +37,17 @@ class TarArchiver(Archiver):
         self._mode = f"w|{compression}"
         self._logger = logging.getLogger(self.__class__.__name__)
 
-    def archive(self, src: Union[str, pathlib.Path]) -> BinaryIO:
+    def archive(
+        self, src: Union[str, pathlib.Path], exclude: List[str] = None
+    ) -> BinaryIO:
         self._logger.info(f"Archiving {src}")
+        if exclude is None:
+            exclude = []
         src = pathlib.Path(src)
         if not src.exists():
             raise FileNotFoundError(f"Source file/folder {src} does not exist")
 
-        with tempfile.TemporaryFile() as tarstream:
-            with tarfile.open(fileobj=tarstream, mode=self._mode) as tar:
-                tar.add(src, arcname=src.name)
-                pass
-            return tarstream
+        tarstream = tempfile.TemporaryFile()
+        with tarfile.open(fileobj=tarstream, mode=self._mode) as tar:
+            tar.add(src, filter=create_filter(exclude))
+        return tarstream
